789ffab9260a41158108a329f5976e8b1de11808
Rebecca Walsh <rawalsh@ucsd.edu>
2014-02-26 22:33:46 -0800

    Alternative redesign

--- public/js/custom.js
+++ public/js/custom.js
@@ -8,9 +8,9 @@ $(window).load(function(){
   $(".twentytwenty-container").twentytwenty();
 });
 
-    function swapButton() {
-      var b = $('#button');
-      b.text('Done');
-      b.buttonMarkup({ icon: "check" });
-      b.button('refresh');
-    }
+function swapButton() {
+    var b = $('#button');
+    b.text('Done');
+    b.buttonMarkup({ icon: "check" });
+    b.button('refresh');
+}
